ffc7a99cc03338720641dd4c22fef2276ed1cc97
sfahadh <sfahadh14@gmail.com>
2020-12-12 18:43:14 -0500

    created news-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { NewsListComponent } from './components/news-list/news-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    NewsListComponent
   ],
   imports: [
     BrowserModule,
