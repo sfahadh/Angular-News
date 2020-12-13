2e35da7b96a27343fa61abc92c189856f392b325
sfahadh <sfahadh14@gmail.com>
2020-12-12 19:10:25 -0500

    added a way to fetch data from top headlines in news service

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -3,7 +3,15 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+  
 export class NewsService {
+  private API_KEY = "fb2632501a6a4372b832feb972f3c5a2";
+  private URL = "https://newsapi.org/v2/";
+  private categories = ["top-headlines", "everything", "sources"];
 
   constructor() { }
+
+  getTopHeadlines(country: string) {
+    return `${this.URL}/top-headlines?country=${country}&apiKey=${this.API_KEY}`;
+  }
 }
