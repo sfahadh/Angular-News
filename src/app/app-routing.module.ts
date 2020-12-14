87ef881140bd10f5743d162c255cc83d59f8b9a2
sfahadh <sfahadh14@gmail.com>
2020-12-14 15:26:31 -0500

    added routes to each category

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,14 @@ import { NewsListComponent } from './components/news-list/news-list.component';
 
 const routes: Routes = [
   { path: '', component: NewsListComponent },
-  { path: 'business', component: NewsListComponent }
+  { path: 'business', component: NewsListComponent },
+  { path: 'politics', component: NewsListComponent },
+  { path: 'science', component: NewsListComponent },
+  { path: 'technology', component: NewsListComponent },
+  { path: 'sports', component: NewsListComponent },
+  { path: 'health', component: NewsListComponent },
+  { path: 'finance', component: NewsListComponent },
+  { path: 'entertainment', component: NewsListComponent }
 ];
 
 @NgModule({
