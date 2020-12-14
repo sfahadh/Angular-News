cc3fe7da902712f553bb3ef482c1003a9e7bada4
sfahadh <sfahadh14@gmail.com>
2020-12-14 14:20:28 -0500

    created news article model

---
+++ src/app/models/article.ts
@@ -0,0 +1,13 @@
+export class Article {
+    author: string;
+    content: string;
+    description: string;
+    publishedAt: string;
+    source: {
+        id: string;
+        name: string;
+    };
+    title: string;
+    url: string;
+    urlToImage: string;
+}
