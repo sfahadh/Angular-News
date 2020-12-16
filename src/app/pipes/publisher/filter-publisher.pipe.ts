057314dff838992296d5b82e6710c9a397a0e7ca
sfahadh <sfahadh14@gmail.com>
2020-12-16 18:14:06 -0500

    articles are successfully filtered by publisher

--- src/app/pipes/publisher/filter-publisher.pipe.ts
+++ src/app/pipes/publisher/filter-publisher.pipe.ts
@@ -1,12 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Article } from 'src/app/models/article';
 
 @Pipe({
   name: 'filterPublisher'
 })
+  
 export class FilterPublisherPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(articles: Article[], publisher: string): any {
+    if (publisher === undefined || publisher === "") return articles;
+    return articles.filter(article => article.source.name.toLowerCase().startsWith(publisher.toLowerCase()));
   }
 
 }
--- src/app/pipes/title/filter-title.pipe.ts
+++ src/app/pipes/title/filter-title.pipe.ts
@@ -9,9 +9,7 @@ export class FilterTitlePipe implements PipeTransform {
 
   transform(articles: Article[], title: string): unknown {
     if (title === undefined || title === "") return articles;
-    return articles.filter(article => {
-      return article.title.toLowerCase().startsWith(title.toLowerCase());
-    });
+    return articles.filter(article => article.title.toLowerCase().startsWith(title.toLowerCase()));
   }
 
 }
